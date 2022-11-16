4676002d593738eb49fc487472e4399263c8def4
Yuma Shimizu <e225741@ie.u-ryukyu.ac.jp>
2022-11-16 18:35:37 +0900

    step2

--- app/src/main/java/jp/ac/uryukyu/ie/e225741/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225741/Enemy.java
@@ -34,9 +34,11 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
+        if (this.dead == false){
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
