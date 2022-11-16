115879761a9ddbd8b7d838cac2e0f4730e22740c
Yuma Shimizu <e225741@ie.u-ryukyu.ac.jp>
2022-11-16 20:06:17 +0900

    カプセル化完了

--- app/src/main/java/jp/ac/uryukyu/ie/e225741/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225741/Enemy.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e225741;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -36,7 +36,7 @@ public class Enemy {
     public void attack(Hero hero){
         if (this.dead == false){
             int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
         }
     }
@@ -53,5 +53,21 @@ public class Enemy {
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+
+    public boolean getDead(){
+        return this.dead;
+    }
+    
+    
+    
 
 }
--- app/src/test/java/jp/ac/uryukyu/ie/e225741/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e225741/EnemyTest.java
@@ -19,6 +19,6 @@ class EnemyTest {
             demoHero.attack(slime); // over kill
         }
         slime.attack(demoHero);
-        assertEquals(defaultHeroHp, demoHero.hitPoint);
+        assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
 }
